d73b553a4e19adb43ee04f1d3780cff80b8a82f7
Erik Peteri <91789264+LerkkaP@users.noreply.github.com>
2022-06-16 20:36:53 +0300

    Update pong.py

--- pong.py
+++ pong.py
@@ -1,4 +1,5 @@
 import pygame
+from random import randint
 
 class Main():
     def __init__(self):
@@ -27,6 +28,9 @@ class Main():
         self.ylos2 = False
         self.alas2 = False
 
+        self.nopeusx = 4
+        self.nopeusy = 4
+
         self.silmukka()
 
     def lataa_kuvat(self):
@@ -36,27 +40,62 @@ class Main():
 
     def pelaaja1(self):
         if self.ylos1 and self.paikka1 > 0:
-            self.paikka1 -= 5
+            self.paikka1 -= 7
         if self.alas1 and self.paikka1 <= self.korkeus - self.kuvat[0].get_height():
-            self.paikka1 += 5
+            self.paikka1 += 7
 
     def pelaaja2(self):
         if self.ylos2 and self.paikka2 > 0:
-            self.paikka2 -= 5
+            self.paikka2 -= 7
         if self.alas2 and self.paikka2 <= self.korkeus - self.kuvat[1].get_height():
-            self.paikka2 += 5
+            self.paikka2 += 7
 
-    #def pallo_liike(self):
-        #self.palloY += 2
-        #if self.palloY >= self.korkeus - self.kuvat[2].get_height():
-            #self.palloY -= 2
+    def pallo_liike(self):
+
+        tormays1 = self.kuvat[0].get_rect(center = (19 + self.kuvat[0].get_width(), self.paikka1))
+        tormays2 = self.kuvat[1].get_rect(center = (self.leveys - self.kuvat[1].get_width() - 9.1, self.paikka2))
+
+        self.palloX += self.nopeusx
+        self.palloY += self.nopeusy
+        
+        if self.kuvat[2].get_width() + self.palloY >= self.korkeus or self.palloY <= 0:
+            self.nopeusy = - self.nopeusy  
+
+        if tormays2.collidepoint(self.palloX + self.kuvat[2].get_width(), self.palloY):
+            self.nopeusx = - self.nopeusx
+
+        if tormays1.collidepoint(self.palloX + self.kuvat[1].get_width(), self.palloY):
+            self.nopeusx = - self.nopeusx
 
     def lopetusnaytto(self):
+
+        pelaa_uudestaan_valkoinen = self.fontti_suuri.render("Pelaa uudestaan", True, (255, 255, 255))
+        pelaa_uudestaan_musta = self.fontti_suuri.render("Pelaa uudestaan", True, (255, 0, 0))
+
+        teksti = pelaa_uudestaan_valkoinen.get_rect(center = (self.leveys / 2, self.korkeus / 1.6))
+
         while True:
             for tapahtuma in pygame.event.get():
                 if tapahtuma.type == pygame.KEYDOWN:
                     if tapahtuma.key == pygame.K_ESCAPE:
                         exit()
+                
+                if tapahtuma.type == pygame.MOUSEBUTTONDOWN and teksti.collidepoint(pygame.mouse.get_pos()):
+                    Main()
+
+                if tapahtuma.type == pygame.QUIT:
+                    exit()
+
+            self.naytto.fill((0, 0, 0))
+
+            if teksti.collidepoint(pygame.mouse.get_pos()):
+                self.naytto.blit(pelaa_uudestaan_musta, (self.leveys / 2 - pelaa_uudestaan_musta.get_width() / 2, self.korkeus / 1.5 - pelaa_uudestaan_musta.get_height()))
+            else:
+                self.naytto.blit(pelaa_uudestaan_valkoinen, (self.leveys / 2 - pelaa_uudestaan_valkoinen.get_width() / 2, self.korkeus / 1.5 - pelaa_uudestaan_valkoinen.get_height()))
+
+            pygame.display.flip()
+
+            self.kello.tick(60)
 
     def silmukka(self):
         while True:
@@ -75,7 +114,8 @@ class Main():
                 if tapahtuma.key == pygame.K_s:
                     self.alas2 = True
                 if tapahtuma.key == pygame.K_ESCAPE:
-                    exit()
+                    self.lopetusnaytto()
+                    #exit()
 
             if tapahtuma.type == pygame.KEYUP:
                 if tapahtuma.key == pygame.K_UP:
@@ -88,7 +128,7 @@ class Main():
                     self.alas2 = False
 
             if tapahtuma.type == pygame.QUIT:
-                exit()
+                self.lopetusnaytto()
 
     def piirra_naytto(self):
 
